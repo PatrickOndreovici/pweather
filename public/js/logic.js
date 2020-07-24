15cacf07e739e9a905126780733909c2209f2f93
PatrickOndreovici <ondreovicip@gmail.com>
2020-07-24 16:09:09 +0300

    make it better

---
+++ public/js/logic.js
@@ -0,0 +1,123 @@
+
+$(document).ready(function() {
+    fetch("/cities")
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        var engine = new Bloodhound({
+            datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+            queryTokenizer: Bloodhound.tokenizers.whitespace,
+            sorter:function(a, b) { 
+                if (a.searches_cnt > b.searches_cnt){
+                    return -1;
+                }else if (a.searches_cnt < b.searches_cnt){
+                    return 1;
+                }else{
+                    return 0;
+                }
+            },
+            local: data
+         });
+         engine.initialize();
+        $("#multiple-datasets .typeahead").typeahead({
+            hint: true,
+            highlight: true,
+            minLength: 1
+        },
+        {
+            limit: 6,
+            name: 'nba-teams',
+            display: 'name',
+            source: engine
+         });
+    })
+    .catch(function(err){
+        console.error(err);
+    });
+    const apiKey = "10b179455d283bed7aab50907b5e31e3";
+    function GetHour(data){
+        var res = "";
+        for (var i = 11; i <= 15; ++i){
+            res += data[i];
+        }
+        return res;
+    }
+    function GetWeather(url1, url2){
+        fetch(url1)
+            .then(function(response){
+                return response.json();
+            })
+            .then(function(data){
+                $("#location").html(data.name + ", " + data.sys.country);
+                $("#temperature").html(Math.floor(data.main.temp) + " <span>&#8451;</span>");
+                var iconUrl = "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
+                $("#icon").attr("src", iconUrl);
+                $("#description").html(data.weather[0].description);
+                $("#weather").css("display", "inline-block");
+                const data2 = new URLSearchParams();
+                data2.append("cityName", data.name + ", " + data.sys.country);
+                fetch("/city", {
+                    method: 'post',
+                    body: data2
+                });
+
+            })
+            .catch(function(err){
+                console.error(err);
+            });
+        fetch(url2)
+            .then(function(response){
+                return response.json();
+            })
+            .then(function(data){
+                $("#hour1").html(GetHour(data.list[0].dt_txt));
+                $("#hour2").html(GetHour(data.list[1].dt_txt));
+                $("#hour3").html(GetHour(data.list[2].dt_txt));
+                $("#temp1").html(Math.floor(data.list[0].main.temp) + " <span>&#8451;</span>");
+                $("#temp2").html(Math.floor(data.list[1].main.temp) + " <span>&#8451;</span>");
+                $("#temp3").html(Math.floor(data.list[2].main.temp) + " <span>&#8451;</span>");
+                iconUrl = "http://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + "@2x.png";
+                $("#icon1").attr("src", iconUrl);
+                iconUrl = "http://openweathermap.org/img/wn/" + data.list[1].weather[0].icon + "@2x.png";
+                $("#icon2").attr("src", iconUrl);
+                iconUrl = "http://openweathermap.org/img/wn/" + data.list[2].weather[0].icon + "@2x.png";
+                $("#icon3").attr("src", iconUrl);
+            })
+            .catch(function(err){
+                console.error(err);
+            });
+    }
+    function GetWeatherByCity(city){
+        var url1 = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" + "&appid=" + apiKey;
+        var url2 = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=metric" + "&appid=" + apiKey;
+        GetWeather(url1, url2);
+    }
+    function GetCurrentCityWeather(){
+        function success(position) {
+            const latitude  = position.coords.latitude;
+            const longitude = position.coords.longitude;
+            var url1 = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&units=metric" + "&appid=" + apiKey;
+            var url2 = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&units=metric" + "&appid=" + apiKey;
+            GetWeather(url1, url2);
+          }
+
+          function error() {
+    
+          }
+        
+          if(!navigator.geolocation) {
+    
+          } else {
+    
+            navigator.geolocation.getCurrentPosition(success, error);
+          }
+    }
+
+    GetCurrentCityWeather();
+    $("#search").submit(function(){
+        var city = $(this).serializeArray()[0].value;
+        GetWeatherByCity(city);
+        return false;
+    });
+});
---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+mongoose.connect('mongodb+srv://PatrickOndreovici:PatrickPatrick1@cluster0.fhml9.mongodb.net/WeatherApp?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+const City = mongoose.model('cities', { name: String, searches_cnt: Number}, 'cities');
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.get('/cities', function(req, res){
+    City.find({}, function(err, users) {
+        res.send(users);  
+      });
+});
+
+app.post('/city', function(req, res){
+    var cityName = req.body.cityName;
+    City.find({name : cityName}, function (err, docs) {
+        if (docs.length == 1){
+            console.log("DA");
+            City.findOneAndUpdate(
+                {"name":cityName}, 
+                { 
+                    $inc: {'searches_cnt':1}
+                },
+                {
+                    returnNewDocument: true
+                }
+            , function( error, result){
+                // In this moment, you recive a result object or error
+            
+                // ... Your code when have result ... //
+            });
+        }else{
+            const city = new City({ name: cityName, searches_cnt: 1}); 
+            city.save();
+        }
+    });
+    res.send("succes");
+});
+
+app.listen(process.env.PORT || 3000, () => console.log(`Example app listening at http://localhost:${port}`))
